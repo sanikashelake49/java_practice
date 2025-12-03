75dabf7a3de7a50c9cec91b7372157d08d5aec65
Sanika <shelakesanika370@gmail.com>
2025-12-03 18:25:59 +0530

    this is parent class

---
+++ parent.java
@@ -0,0 +1,22 @@
+class child
+ {
+   public static void main(String[]args)
+     {
+      
+       System.out.println("child class");
+
+    }
+
+ }
+
+
+class parent
+   {
+   public static void main(String[]args)
+     {
+
+     System.out.println("parent class");  
+
+
+   }
+}
