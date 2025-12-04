43f11532abd22dcc773e0a763d7333aa8d05b33d
Sanika <shelakesanika370@gmail.com>
2025-12-04 12:51:58 +0530

    new file

---
+++ StudentData.java
@@ -0,0 +1,24 @@
+
+class StudentData {
+	
+	int stdId;
+	String  stdName;
+	int phy_Mrk;
+	int chem_Mrk;
+	
+	public StudentData() {
+		super();
+	}
+
+	public StudentData(int stdId, String stdName, int phy_Mrk, int chem_Mrk) {
+		super();
+		this.stdId = stdId;
+		this.stdName = stdName;
+		this.phy_Mrk = phy_Mrk;
+		this.chem_Mrk = chem_Mrk;
+	}
+    
+	
+	
+	
+}
